132af41b3a5798b887b43d4747a923f545c468e8
Sabareeswaran B <sabareeswaran2510@gmail.com>
2022-03-16 13:37:24 +0530

    Exceptional handling for book controller

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -20,30 +20,58 @@ namespace LibraryMS.Controllers
 
         // GET: api/Book
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Book>>> GetBook()
+        public async Task<IActionResult> GetAllBooks()
         {
-            return await _context.Book.ToListAsync();
+            try
+            {
+                var books = await _context.Book.Where(w => w.Active == "true").ToListAsync();
+                return Ok(
+                    new { status = "Success", message = "Get all books successfully", data = books }
+                );
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine("get all books exception: {0}", ex);
+                return BadRequest(new { status = "failed", message = ex.Message });
+            }
         }
 
         // GET: api/Book/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Book>> GetBook(int id)
+        public async Task<IActionResult> GetBookById(Guid id)
         {
-            var book = await _context.Book.FindAsync(id);
-
-            if (book == null)
+            try
             {
-                return NotFound();
-            }
+                var book = await _context.Book
+                    .Where(w => w.BookId == id)
+                    .Where(w => w.Active == "true")
+                    .FirstAsync();
+                if (book == null)
+                {
+                    return NotFound(new { status = "failed", message = "Book not found!" });
+                }
 
-            return book;
+                return Ok(
+                    new
+                    {
+                        status = "Success",
+                        message = "Get single books successfully",
+                        data = book
+                    }
+                );
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine("get book by id exception: {0}", ex);
+                return BadRequest(new { status = "failed", message = ex.Message });
+            }
         }
 
         // PUT: api/Book/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [Authorize(Role.Admin)]
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutBook(Guid id, Book book)
+        public async Task<IActionResult> UpdateExistingBook(Guid id, Book book)
         {
             if (id != book.BookId)
             {
@@ -55,69 +83,123 @@ namespace LibraryMS.Controllers
             try
             {
                 await _context.SaveChangesAsync();
+                var updatedBook = await _context.Book.FindAsync(id);
+                return Ok(
+                    new
+                    {
+                        status = "Success",
+                        message = "Book updated successfully",
+                        data = updatedBook
+                    }
+                );
             }
-            catch (DbUpdateConcurrencyException)
+            catch (DbUpdateConcurrencyException ex)
             {
                 if (!BookExists(id))
                 {
-                    return NotFound();
+                    return NotFound(new { status = "failed", message = "Book not found" });
                 }
                 else
                 {
-                    throw;
+                    Console.WriteLine("update existing book exception: {0}", ex);
+                    return BadRequest(new { status = "failed", message = ex.Message });
                 }
             }
-
-            return NoContent();
-        }
-        //Put: AddStock/:id/:CopiesAvailable
-        [Authorize(Role.Admin,Role.Clerk)]
-        [HttpPut("{id}/{CopiesAvailable}")]
-        public async Task<ActionResult<Book>> AddStock(Guid id, [FromRoute]int CopiesAvailable)
-        {
-            Book book = await _context.Book.FindAsync(id);
-            book.CopiesAvailable += CopiesAvailable;
-            await _context.SaveChangesAsync();
-            return Ok(book);
         }
 
-        //Put: RemoveStock/:id/:CopiesAvailable
-        [Authorize(Role.Admin,Role.Clerk)]
-        [HttpPut("{id}/{CopiesAvailable}")]
-        public async Task<ActionResult<Book>> RemoveStock(Guid id, [FromRoute]int CopiesAvailable)
+        //Put: AddStock/:id/:CopiesToAdd
+        [Authorize(Role.Admin, Role.Clerk)]
+        [HttpPut("{id}/{CopiesToAdd}")]
+        public async Task<IActionResult> AddStock(Guid id, [FromRoute] int CopiesToAdd)
         {
-            Book book = _context.Book.Where(data => data.BookId == id).FirstOrDefault();
-            book.CopiesAvailable -= CopiesAvailable;
-            await _context.SaveChangesAsync();
-            return Ok(book);
+            try
+            {
+                Book book = await _context.Book.FindAsync(id);
+                if (book == null)
+                    return NotFound(new { status = "failed", message = "Book not found" });
+
+                book.CopiesAvailable += CopiesToAdd;
+                await _context.SaveChangesAsync();
+                return Ok(
+                    new { status = "Success", message = "Added stock successfully", data = book }
+                );
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine("Add stock exception: {0}", ex);
+                return BadRequest(new { status = "failed", message = ex.Message });
+            }
+        }
+
+        //Put: RemoveStock/:id/:CopiesToRemove
+        [Authorize(Role.Admin, Role.Clerk)]
+        [HttpPut("{id}/{CopiesToRemove}")]
+        public async Task<IActionResult> RemoveStock(Guid id, [FromRoute] int CopiesToRemove)
+        {
+            try
+            {
+                Book book = _context.Book.Where(data => data.BookId == id).FirstOrDefault();
+
+                if (book == null)
+                    return NotFound(new { status = "failed", message = "Book not found" });
+
+                book.CopiesAvailable -= CopiesToRemove;
+                await _context.SaveChangesAsync();
+                return Ok(
+                    new { status = "Success", message = "Removed stock successfully", data = book }
+                );
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine("Remove stock exception: {0}", ex);
+                return BadRequest(new { status = "failed", message = ex.Message });
+            }
         }
 
         // POST: api/Book
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [Authorize(Role.Admin)]
         [HttpPost]
-        public async Task<ActionResult<Book>> PostBook(Book book)
+        public async Task<IActionResult> AddNewBook(Book book)
         {
-            _context.Book.Add(book);
-            await _context.SaveChangesAsync();
+            try
+            {
+                book.Active = "true";
+                _context.Book.Add(book);
+                await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetBook", new { id = book.BookId }, book);
+                return CreatedAtAction("GetBook", new { id = book.BookId }, book);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine("Add new book exception: {0}", ex);
+                return BadRequest(new { status = "failed", message = ex.Message });
+            }
         }
+
         [Authorize(Role.Admin)]
         // DELETE: api/Book/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteBook(int id)
         {
-            var book = await _context.Book.FindAsync(id);
-            if (book == null)
+            try
             {
-                return NotFound();
+                var book = await _context.Book.FindAsync(id);
+                if (book == null)
+                {
+                    return NotFound(new { status = "failed", message = "Book not found" });
+                }
+
+                book.Active = "false";
+                await _context.SaveChangesAsync();
+
+                return Ok(new { status = "Success", message = "Book deleted successfully" });
             }
-
-            _context.Book.Remove(book);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            catch (System.Exception ex)
+            {
+                Console.WriteLine("Delete Book exception: {0}", ex);
+                return BadRequest(new { status = "failed", message = ex.Message });
+            }
         }
 
         private bool BookExists(Guid id)
