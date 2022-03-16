5b9ae557b08c48f1e26935babf62bc19c76b4115
Sabareeswaran B <sabareeswaran2510@gmail.com>
2022-03-16 13:37:43 +0530

    Exceptional handling for account controller

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -22,21 +22,37 @@ public class AccountController : ControllerBase
     [HttpPost]
     public IActionResult Login(AuthRequest model)
     {
-        var user = _context.Employee.FirstOrDefault(x => x.EmployeeEmail == model.Email);
-        if (user == null)
-            return BadRequest(new { message = "Email not found!" });
-        var verify = BCrypt.Net.BCrypt.Verify(model.Password, user!.Password);
-        if (!verify)
-            return BadRequest(new { message = "Incorrect password!" });
-        var response = _userServices.Authenticate(user);
-        return Ok(response);
+        try
+        {
+            var user = _context.Employee.FirstOrDefault(x => x.EmployeeEmail == model.Email);
+            if (user == null)
+                return BadRequest(new { status = "failed", message = "Email not found!" });
+            var verify = BCrypt.Net.BCrypt.Verify(model.Password, user!.Password);
+            if (!verify)
+                return BadRequest(new { status = "failed", message = "Incorrect password!" });
+            var response = _userServices.Authenticate(user);
+            return Ok(new { status = "success", message = "Login Successfull", data = response });
+        }
+        catch (System.Exception ex)
+        {
+            Console.WriteLine("login exception: {0}", ex);
+            return BadRequest(new { status = "failed", message = ex.Message });
+        }
     }
 
     [Authorize(Role.Admin)]
     [HttpGet]
     public IActionResult GetAllUsers()
     {
-        var users = _userServices.GetAll();
-        return Ok(users);
+        try
+        {
+            var users = _userServices.GetAll();
+            return Ok(new { status = "success", message = "Get all users Successfull", data = users });
+        }
+        catch (System.Exception ex)
+        {
+            Console.WriteLine("get all users exception: {0}", ex);
+            return BadRequest(new { status = "failed", message = ex.Message });
+        }
     }
 }
